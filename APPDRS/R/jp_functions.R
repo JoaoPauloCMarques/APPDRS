d62844dc13284b991c3bd75a68a73e8265cffc11
JoaoPauloCMarques <113448622+JoaoPauloCMarques@users.noreply.github.com>
2022-09-18 15:27:54 -0300

    Add files via upload

---
+++ APPDRS/R/jp_functions.R
@@ -0,0 +1,41 @@
+# a<-c(car$speed)
+# a<-table(car$speed)
+# a<-a[which.max(a)]
+# a<-names(a)
+# as.numeric(a)
+
+law <- function(x,y,z){
+  a<-rep("Sim", times=x)
+  b<-rep("Não", times=y)
+  populacao<-c(a,b)
+  b<-c()
+  for(i in 1:z){
+    n<-sample(populacao,i)
+    j<-table(n)
+    if (length(j) == 1 & names(j)[1] == "Não"  ){
+      porcentagem_de_sim <- 0
+    }else if(length(j) == 1 & names(j)[1] == "Sim") {
+      porcentagem_de_sim <- 100
+    }else if (length(j) == 2) { porcentagem_de_sim <- ( (j[2]/sum(j)) ) * 100}
+    b<-append(b,porcentagem_de_sim)
+  }
+  return(b)
+}
+
+jp_mode<-function(x){
+  a<-table(x)
+  b<-sort(a, decreasing = TRUE)
+  c<-b[which.max(b)]
+  d<-b[-which.max(b)]
+  if(any(c==d) ==  TRUE){
+    texto1<-c("Há mais de uma moda!\n")
+    texto2<-c("Linha 1: escores.\n")
+    texto3<-c("Linha 2: número de repetição.\n")
+    cat(texto1)
+    cat(texto2)
+    cat(texto3)
+    print(b)
+  }else if(any(c==d) ==  FALSE){
+    return(as.numeric(names(c)))
+  }
+}
